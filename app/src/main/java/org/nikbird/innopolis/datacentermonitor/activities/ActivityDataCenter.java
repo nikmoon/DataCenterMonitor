bc8b42c2eeb30914db77c5e499237dd6acf2cf9e
nikbird <nikbird@debian>
2017-07-13 08:34:57 +0300

    Drawing racks and servers in progress

--- app/src/main/java/org/nikbird/innopolis/datacentermonitor/activities/ActivityDataCenter.java
+++ app/src/main/java/org/nikbird/innopolis/datacentermonitor/activities/ActivityDataCenter.java
@@ -1,11 +1,18 @@
 package org.nikbird.innopolis.datacentermonitor.activities;
 
+import android.view.View;
+
 import org.nikbird.innopolis.datacentermonitor.R;
 import org.nikbird.innopolis.datacentermonitor.abstractclasses.ActivityListener;
 import org.nikbird.innopolis.datacentermonitor.interfaces.IDataCenter;
 import org.nikbird.innopolis.datacentermonitor.interfaces.IServer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ActivityDataCenter extends ActivityListener {
+
+    private Map<IServer, View> mServerViews;
 
     @Override
     public void onServerStateChanged(IServer server, IServer.State statePrev) {
@@ -15,7 +22,9 @@ public class ActivityDataCenter extends ActivityListener {
     @Override
     public void onReplicationComplete(IDataCenter dataCenter) {
         setContentView(R.layout.activity_data_center);
-        // отрисовываем стойки и сервера
         mDataCenter.setEventListener(this);
+        mServerViews = new HashMap<>();
+
+        // отрисовываем стойки и сервера
     }
 }
